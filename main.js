139dde0c2a1071a34da3d3f69008807bda4f1815
CodeWithPrena-28 <prenaroghani@gmail.com>
2024-07-29 15:54:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question 1
+console.log("HELLO WORLD, I am Prena !");
+// Question 2
+var myName = "Prena";
+var message = "is a Innoxent Girl";
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Question 1
+console.log("HELLO WORLD, I am Prena !");
+
+// Question 2
+let myName = "Prena";
+let message = "is a Innoxent Girl";
+console.log(myName+" "+message);
